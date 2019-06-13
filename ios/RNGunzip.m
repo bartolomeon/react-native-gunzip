82da72fc832711b85cbddc8cdb3d710e68ffb4cb
Michael Waskosky <mike@tetonapps.com>
2019-06-13 16:44:33 -0600

    implementing gunzip in ios

--- ios/RNGunzip.m
+++ ios/RNGunzip.m
@@ -1,13 +1,47 @@
 
 #import "RNGunzip.h"
 
-@implementation RNGunzip
-
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
-}
-RCT_EXPORT_MODULE()
+@interface RNGunzip ()
 
 @end
-  
+
+@implementation RNGunzip
+
+RCT_EXPORT_MODULE()
+
+RCT_REMAP_METHOD(gunzip,
+                 filePath: (NSString *) source
+                 toFolder: (NSString *) folder
+                 force: (BOOL) force
+                 resolver: (RCTPromiseResolveBlock)resolve
+                 rejecter: (RCTPromiseRejectBlock)reject)
+{
+    NSFileManager *manager = [NSFileManager defaultManager];
+
+    if (![manager fileExistsAtPath:source]) {
+        reject(@"-2", @"file not found", nil);
+        return;
+    }
+
+    if ([manager fileExistsAtPath:folder]) {
+        if (!force) {
+            reject(@"-2", @"folder exists", nil);
+            return;
+        }
+        NSError *unlinkError;
+        if (![manager removeItemAtPath:folder error:&unlinkError]) {
+            reject([@(unlinkError.code) stringValue], unlinkError.localizedDescription, unlinkError);
+            return;
+        }
+    }
+
+    // TODO Passing an error here results in EXC_BAD_ACCESS because the error is released
+    if (![DCTar decompressData:[manager contentsAtPath:source] toPath:folder error:nil]) {
+        reject(@"-3", @"error while decompressing", nil);
+        return;
+    }
+
+    resolve(@{@"path": folder});
+}
+
+@end
